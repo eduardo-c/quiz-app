e4bf2839679df7ba50a91c7d361071e5068f7897
Eduardo Castro <eduardo@Eduardos-MacBook-Air.local>
2024-08-29 16:24:43 -0600

    Shuffling answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,7 +5,15 @@ import QUESTIONS from '../questions.js'
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
+    // It's a good practice to manage as less amount of state values as possible
+    // That's why it's better to derive computed values when possible
     const currentQuestionIndex = userAnswers.length;
+
+    // Shuffling the answers to have a different orfder every time the component gets loaded
+    const shuffledAnswers = [...QUESTIONS[currentQuestionIndex].answers];
+    // sort modifies the order of the array in place, for that reason we had to create a copy from QUESTIONS
+    // sort receives a comparator
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     function handleSelectQuestion(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
@@ -18,7 +26,7 @@ export default function Quiz() {
             <div id="question">
                 <h2>{QUESTIONS[currentQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[currentQuestionIndex].answers.map(answer => (
+                    {shuffledAnswers.map(answer => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectQuestion(answer)}>{answer}</button>
                         </li>
