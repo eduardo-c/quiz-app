6038cdc496b2ed7986bbf76ec6c97011fff58fdb
Eduardo Castro <eduardo@Eduardos-MacBook-Air.local>
2024-08-29 16:39:15 -0600

    Adding logic when the quiz is completed

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import QUESTIONS from '../questions.js'
+import quizCompleteImg from '../assets/quiz-complete.png'
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
@@ -9,17 +10,29 @@ export default function Quiz() {
     // That's why it's better to derive computed values when possible
     const currentQuestionIndex = userAnswers.length;
 
-    // Shuffling the answers to have a different orfder every time the component gets loaded
-    const shuffledAnswers = [...QUESTIONS[currentQuestionIndex].answers];
-    // sort modifies the order of the array in place, for that reason we had to create a copy from QUESTIONS
-    // sort receives a comparator
-    shuffledAnswers.sort(() => Math.random() - 0.5);
+    const isQuizComplete = currentQuestionIndex === QUESTIONS.length;
 
     function handleSelectQuestion(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
     }
+
+    if(isQuizComplete){
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy Icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+
+    // Note that we need to move this derived state array to avoid accessing invalid indexes when the quiz is complete
+    // Shuffling the answers to have a different orfder every time the component gets loaded
+    const shuffledAnswers = [...QUESTIONS[currentQuestionIndex].answers];
+    // sort modifies the order of the array in place, for that reason we had to create a copy from QUESTIONS
+    // sort receives a comparator
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     return (
         <div id="quiz">
